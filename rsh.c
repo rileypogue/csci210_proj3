5cbbb1bac6a56c1d9d9215659caa307cdac610ef
Riley Pogue <pogue@mines.edu>
2024-11-12 20:35:06 -0700

    doing my best

--- rsh.c
+++ rsh.c
@@ -15,8 +15,43 @@ int isAllowed(const char*cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	
+	for (int i = 0; i < N; i++) {
+		if(strcmp(cmd, allowed[i])) {
+			return 1;
+		}
+	}
 	return 0;
+}
+
+void displayHelp() {
+	printf("The allowed commands are:\n");
+	for (int i = 0; i < N; i++) {
+		printf("%d: %s\n", i + 1, allowed[i]);
+	}
+}
+
+void changeDir(char **argv) {
+	if (argv[1] != NULL && argv[2] != NULL) {
+		printf("-rsh: cd: too many command arguments\n");
+	} else if (chdir(argv[1] ? argv[1] : getenv("HOME")) != 0) {
+		perror("-rsh: cd");
+	}
+}
+
+int executeCommand(char **argv) {
+	pid_t pid;
+	int status;
+
+	if (posix_spawnp(&pid, argv[0], NULL, NULL, argv, environ) != 0) {
+		perror("posix_spawnp");
+		return -1;
+	} 
+
+	if (waitpid(pid, &status, 0) == -1) {
+		perror("waitpid");
+	}
+
+	return status;
 }
 
 int main() {
@@ -25,6 +60,7 @@ int main() {
     // Add variables as needed
 
     char line[256];
+    char *argv[21];
 
     while (1) {
 
@@ -40,7 +76,32 @@ int main() {
 	// Add code to spawn processes for the first 9 commands
 	// And add code to execute cd, exit, help commands
 	// Use the example provided in myspawn.c
+	int argc =  0;
+	char *token = strtok(line, " ");
+	while (token != NULL && argc < 20) {
+		argv[argc++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[argc] = NULL;
 
+	if (argc == 0) {
+		continue; // skipping empty lines
+	}
+
+	if (!isAllowed(argv[0])) {
+		printf("NOT ALLOWED!\n");
+		continue;
+	}
+
+	if (strcmp(argv[0], "exit") == 0) {
+		return 0;
+	} else if (strcmp(argv[0], "help") == 0) {
+		displayHelp();
+	} else if (strcmp(argv[0], "cd") == 0) {
+		changeDir(argv);
+	} else {
+		executeCommand(argv);
+	}
     }
     return 0;
 }
